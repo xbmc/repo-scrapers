49d37495b0bdf1ccd5d218f3a797e41905945fd5
ronie <ronie@kodi.tv>
2020-08-23 01:55:05 +0200

    [metadata.generic.albums] 1.0.12

--- metadata.generic.albums/lib/musicbrainz.py
+++ metadata.generic.albums/lib/musicbrainz.py
@@ -140,14 +140,12 @@ def musicbrainz_albumart(data):
             thumbdata['aspect'] = 'thumb'
             thumbs.append(thumbdata)
         if 'Back' in item['types']:
-            albumdata['back'] = item['image']
             backdata = {}
             backdata['image'] = item['image']
             backdata['preview'] = item['thumbnails']['small']
             backdata['aspect'] = 'back'
             extras.append(backdata)
         if 'Medium' in item['types']:
-            albumdata['discart'] = item['image']
             discartdata = {}
             discartdata['image'] = item['image']
             discartdata['preview'] = item['thumbnails']['small']
@@ -155,7 +153,6 @@ def musicbrainz_albumart(data):
             extras.append(discartdata)
         # exculde spine+back images
         if 'Spine' in item['types'] and len(item['types']) == 1:
-            albumdata['spine'] = item['image']
             spinedata = {}
             spinedata['image'] = item['image']
             spinedata['preview'] = item['thumbnails']['small']
