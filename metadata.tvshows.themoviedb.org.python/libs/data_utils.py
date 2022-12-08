b538145474931c07189915ab39d328535efa2198
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2022-12-08 02:58:16 -0500

    [metadata.tvshows.themoviedb.org.python] v1.6.0
    - change to use new JSON based episodeguide format to handle multiple providers
    - fixes for last minute changes to new Nexus Python bindings

--- metadata.tvshows.themoviedb.org.python/libs/data_utils.py
+++ metadata.tvshows.themoviedb.org.python/libs/data_utils.py
@@ -23,6 +23,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import re
+import json
 from xbmc import Actor, VideoStreamDetail
 from collections import namedtuple
 from .utils import safe_get, logger
@@ -125,13 +126,18 @@ def _get_directors(episode_info):
 def _set_unique_ids(ext_ids, vtag):
     # type: (Dict, ListItem) -> ListItem
     """Extract unique ID in various online databases"""
+    return_ids = {}
     for key, value in ext_ids.items():
         if key in VALIDEXTIDS and value:
             if key == 'tmdb_id':
                 isTMDB = True
             else:
                 isTMDB = False
-            vtag.setUniqueID(str(value), type=key[:4], isDefault=isTMDB)
+            shortkey = key[:-3]
+            str_value = str(value)
+            vtag.setUniqueID(str_value, type=shortkey, isdefault=isTMDB)
+            return_ids[shortkey] = str_value
+    return return_ids
 
 
 def _set_rating(the_info, vtag):
@@ -148,7 +154,7 @@ def _set_rating(the_info, vtag):
                      (str(rating), str(votes)))
         if rating > 0:
             vtag.setRating(rating, votes=votes,
-                           type=rating_type, isDefault=first)
+                           type=rating_type, isdefault=first)
             first = False
 
 
@@ -169,7 +175,7 @@ def _add_season_info(show_info, vtag):
                 theurl, previewurl = get_image_urls(image)
                 if theurl:
                     vtag.addAvailableArtwork(
-                        theurl, art_type=destination, preview=previewurl, season=season['season_number'])
+                        theurl, arttype=destination, preview=previewurl, season=season['season_number'])
 
 
 def get_image_urls(image):
@@ -198,7 +204,7 @@ def set_show_artwork(show_info, list_item):
                 theurl, previewurl = get_image_urls(image)
                 if image.get('iso_639_1') != None and settings.CATLANDSCAPE and theurl:
                     vtag.addAvailableArtwork(
-                        theurl, art_type="landscape", preview=previewurl)
+                        theurl, arttype="landscape", preview=previewurl)
                 elif theurl:
                     fanart_list.append({'image': theurl})
             if fanart_list:
@@ -214,7 +220,7 @@ def set_show_artwork(show_info, list_item):
                 theurl, previewurl = get_image_urls(image)
                 if theurl:
                     vtag.addAvailableArtwork(
-                        theurl, art_type=destination, preview=previewurl)
+                        theurl, arttype=destination, preview=previewurl)
     return list_item
 
 
@@ -234,14 +240,14 @@ def add_main_show_info(list_item, show_info, full_info=True):
     vtag.setPlot(plot)
     vtag.setPlotOutline(plot)
     vtag.setMediaType('tvshow')
-    vtag.setEpisodeGuide(str(show_info['id']))
+    ext_ids = {'tmdb_id': show_info['id']}
+    ext_ids.update(show_info.get('external_ids', {}))
+    epguide_ids = _set_unique_ids(ext_ids, vtag)
+    vtag.setEpisodeGuide(json.dumps(epguide_ids))
     if show_info.get('first_air_date'):
         vtag.setYear(int(show_info['first_air_date'][:4]))
         vtag.setPremiered(show_info['first_air_date'])
     if full_info:
-        ext_ids = {'tmdb_id': show_info['id']}
-        ext_ids.update(show_info.get('external_ids', {}))
-        _set_unique_ids(ext_ids, vtag)
         vtag.setTvShowStatus(safe_get(show_info, 'status', ''))
         genre_list = safe_get(show_info, 'genres', {})
         genres = []
@@ -292,7 +298,7 @@ def add_main_show_info(list_item, show_info, full_info=True):
             theurl = settings.IMAGEROOTURL + image
             previewurl = settings.PREVIEWROOTURL + image
             vtag.addAvailableArtwork(
-                theurl, art_type='poster', preview=previewurl)
+                theurl, arttype='poster', preview=previewurl)
     logger.debug('adding tv show information for %s to list item' % showname)
     return list_item
 
@@ -331,7 +337,7 @@ def add_episode_info(list_item, episode_info, full_info=True):
             theurl, previewurl = get_image_urls(image)
             if theurl:
                 vtag.addAvailableArtwork(
-                    theurl, art_type='thumb', preview=previewurl)
+                    theurl, arttype='thumb', preview=previewurl)
         vtag.setWriters(_get_credits(episode_info))
         vtag.setDirectors(_get_directors(episode_info))
     logger.debug('adding episode information for S%sE%s - %s to list item' %
@@ -365,7 +371,7 @@ def parse_nfo_url(nfo):
                     show_id_match.group(1), show_id_match.group(2))
             if tmdb_id:
                 logger.debug('match group 3: ' + str(ep_grouping))
-                sid_match = UrlParseResult('themoviedb', tmdb_id, ep_grouping)
+                sid_match = UrlParseResult('tmdb', tmdb_id, ep_grouping)
                 break
     return sid_match, ns_match
 
@@ -378,8 +384,12 @@ def _convert_ext_id(ext_provider, ext_id):
                       'tvdb': 'tvdb_id'}
     show_url = FIND_URL.format(ext_id)
     params = TMDB_PARAMS.copy()
-    params['external_source'] = providers_dict[ext_provider]
-    show_info = api_utils.load_info(show_url, params=params)
+    provider = providers_dict.get(ext_provider)
+    if provider:
+        params['external_source'] = provider
+        show_info = api_utils.load_info(show_url, params=params)
+    else:
+        show_info = None
     if show_info:
         tv_results = show_info.get('tv_results')
         if tv_results:
